b04ce41332133fe375804c36a0fdf0c2ca407c7b
1JessIT <maratova.zha@gmail.com>
2023-10-07 17:02:01 +0600

    Header, search, favorites, login, add, localization

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 import MainLayouts from "./layouts/MainLayouts";
-import { theme } from "./theme";
-import { store } from "./store";
-import { CssBaseline } from "@mui/material";
 
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <BrowserRouter>
-            <Routes>
-              <Route element={<MainLayouts />}>
-                <Route path="/" />
-                <Route path="/" />
-                <Route path="/" />
-                <Route path="/" />
-                <Route path="/" />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </ThemeProvider>
-      </Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayouts />}>
+            <Route path="/" />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -3,11 +3,17 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "#f5f5f5",
+      main: "#ad1457",
+      secondary: "#d1c4e9",
     },
 
     secondary: {
-      main: "#050505",
+      main: "#ffc107",
+      secondary: "#4527a0",
     },
+  },
+
+  fonts: {
+    Nunito: "Nunito, sans-serif",
   },
 });
